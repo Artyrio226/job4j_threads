73612ddbd1c64ba032bc4ae3990b20d7c8e2bb02
Artyrio226 <artur_sar_80@mail.ru>
2023-11-07 16:11:07 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        first.start();
+        second.start();
+
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
+            if (first.getState() == Thread.State.TERMINATED
+                    && second.getState() == Thread.State.TERMINATED) {
+                break;
+            }
+        }
+        System.out.println("Работа завершена.");
+    }
+}
