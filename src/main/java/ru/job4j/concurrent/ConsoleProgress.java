2e31ec690eda072f572fb6ec22cc8ac16eb01937
Artyrio226 <artur_sar_80@mail.ru>
2023-11-07 18:06:18 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,31 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        int rotator = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            var process = new char[] {'-', '\\', '|', '/'};
+            System.out.print("\rLoading ... " + process[rotator % 4]);
+            rotator++;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        progress.interrupt();
+    }
+}
